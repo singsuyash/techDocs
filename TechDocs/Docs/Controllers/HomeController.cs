b6875c9bff851e3eb9ea468188bf32f400115840
Suyash Singh <singsuyash@outlook.com>
2015-08-29 23:30:23 +0530

    Site UI changes, social share buttons, contact buttons, social share button not working fine.

--- TechDocs/Docs/Controllers/HomeController.cs
+++ TechDocs/Docs/Controllers/HomeController.cs
@@ -12,17 +12,5 @@ namespace Docs.Controllers
         {
             return View();
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            return View();
-        }
     }
 }
